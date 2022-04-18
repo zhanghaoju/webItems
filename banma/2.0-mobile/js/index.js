bfe2fd16bd043589014593a27dd9d86370249963
zhanghaoju <1348632830@qq.com>
2022-04-18 22:15:44 +0800

    20220418

--- banma/2.0-mobile/js/index.js
+++ banma/2.0-mobile/js/index.js
@@ -3,4 +3,7 @@ $(function(){
 	$('.newsModule').on('click',function(){
 		$(this).addClass('animated pulse').siblings('.newsModule').removeClass('pulse');
 	})
+	$('.itHelperModule').on('click',function(){
+		$(this).addClass('animated pulse').siblings('.itHelperModule').removeClass('pulse');
+	})
 });
